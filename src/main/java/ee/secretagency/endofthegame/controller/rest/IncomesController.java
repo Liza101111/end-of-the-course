d2d52b6fbed6f08a470764d428a84214fd918ef3
Liza <lixia101111@gmail.com>
2022-11-11 09:16:19 +0200

    with exception handing by hand

--- src/main/java/ee/secretagency/endofthegame/controller/rest/IncomesController.java
+++ src/main/java/ee/secretagency/endofthegame/controller/rest/IncomesController.java
@@ -3,6 +3,7 @@ package ee.secretagency.endofthegame.controller.rest;
 import ee.secretagency.endofthegame.entity.Income;
 import ee.secretagency.endofthegame.service.IncomesService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,10 @@ import java.util.List;
 @RequestMapping("/api")
 public class IncomesController {
 
-    private final IncomesService service;
+    private final IncomesService incomesService;
 
     public IncomesController(IncomesService service) {
-        this.service = service;
+        this.incomesService = service;
     }
 
     // TODO: handle paging and sorting
@@ -26,17 +27,20 @@ public class IncomesController {
     public List<Income> getAllIncomes() {
         log.info("getting all incomes");
 
-        return service.readAllIncomes();
+        return incomesService.readAllIncomes();
     }
 
     @GetMapping("/incomes/{id}")
-    public Income getIncomeById(@PathVariable("id") Long id){
+    public ResponseEntity<Income> getIncomeById(@PathVariable("id") Long id) {
         log.info("trying to get income with id: [{}]", id);
-        return null;
+        Income income = incomesService.readIncomeById(id);
+        boolean exists = income != null;
+
+        if (exists) {
+            return ResponseEntity.ok(income);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
-
-
-
-
 
 }
--- src/main/java/ee/secretagency/endofthegame/service/IncomesService.java
+++ src/main/java/ee/secretagency/endofthegame/service/IncomesService.java
@@ -5,23 +5,46 @@ import ee.secretagency.endofthegame.repository.IncomesRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
 @Slf4j
 public class IncomesService {
 
-    private final IncomesRepository repository;
+    private final IncomesRepository incomesRepository;
 
-    public IncomesService(IncomesRepository repository) {
-        this.repository = repository;
+    public IncomesService(IncomesRepository incomesRepository) {
+        this.incomesRepository = incomesRepository;
     }
 
     public List<Income> readAllIncomes() {
 //        List<Income> result = repository.findAll();
-        var incomesFromDb = repository.findAll();
+        var incomesFromDb = incomesRepository.findAll();
 
         log.info("incomes from datasource: {}", incomesFromDb);
         return incomesFromDb;
     }
+
+    public Income readIncomeById(Long id) {
+        log.info("reading income with id: [{}]", id);
+        Income income = null;
+        try{
+            income = incomesRepository.getOne(id);
+            if(income == null){
+                log.info("It's null");
+            }else{
+                log.info("It's not mull");
+            }
+            log.info("" + income);
+            log.info("read income: [{}]", income);
+
+        }catch(EntityNotFoundException e){
+            log.warn("some unexpected exception has occurred");
+            return null;
+        }
+        return income;
+    }
+
+
 }
