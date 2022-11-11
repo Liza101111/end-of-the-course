f18f26928687a66ddb3f5757a0b9d73fc11c8fcf
Liza <lixia101111@gmail.com>
2022-11-11 23:49:55 +0200

    [refactor] - adding better class - ErrorInfo - for handing exceptional situation

--- src/main/java/ee/secretagency/endofthegame/controller/rest/IncomeAdvisor.java
+++ src/main/java/ee/secretagency/endofthegame/controller/rest/IncomeAdvisor.java
@@ -1,19 +1,30 @@
 package ee.secretagency.endofthegame.controller.rest;
 
+import ee.secretagency.endofthegame.dto.ErrorInfo;
 import ee.secretagency.endofthegame.exception.IncomeNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.time.ZonedDateTime;
 
 @RestControllerAdvice
 @Slf4j
 public class IncomeAdvisor {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(IncomeNotFoundException.class)
-    public String handleIncomeNotFoundException() {
-        log.info("handing not found incomes");
-        return "cannot find this income";
+    public ErrorInfo handleIncomeNotFoundException(IncomeNotFoundException e) {
+        log.debug("handing not found incomes");
+        log.warn("income not found!", e);
+        return ErrorInfo.builder()
+                .status(404)
+                .error("income not found!")
+                .message(e.getMessage())
+                .timestamp(ZonedDateTime.now())
+                .path(ServletUriComponentsBuilder.fromCurrentRequest().toUriString()) //TODO: extract only path
+                .build();
     }
 }
