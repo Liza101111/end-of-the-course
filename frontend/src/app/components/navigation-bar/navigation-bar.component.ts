66f916a55b6f08a86310b332c5afae7aba3f4f5c
Liza <lixia101111@gmail.com>
2022-11-24 10:38:19 +0200

    [refactor] - using one place for storing urls

--- frontend/src/app/components/navigation-bar/navigation-bar.component.ts
+++ frontend/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {expenseListUrl, homeUrl, incomeListUrl} from "../../models/urls";
+
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-bar.component.css']
 })
 export class NavigationBarComponent implements OnInit {
+
+  home = homeUrl
+  incomes = incomeListUrl
+  expenses = expenseListUrl
+
+
 
   constructor() { }
 
